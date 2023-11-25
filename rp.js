e30307816ec6768ec964a1d411fdefa536fd86b0
Sigrajavascript1717 <152014915+Sigrajavascript1717@users.noreply.github.com>
2023-11-25 10:56:01 -0800

    belajar js pertama

---
+++ rp.js
@@ -0,0 +1,26 @@
+let a ="a"
+let b ="b"
+const c ="c"
+
+let nama ="ega"
+let umur =23
+let apaBenar =true
+let duit =200.3
+
+let angka1 =10
+let angka2 =20
+const hasil = angka1+angka2
+console.log(hasil)
+
+let namaGw ="sigra"
+let namaPanggilan ="ega"
+let jadiNamaGw = namaGw+" "+namaPanggilan
+console.log(jadiNamaGw)
+console.log(namaGw-namaPanggilan)
+
+let uhuy =6
+let manusia=1
+while(manusia<=uhuy){
+    console.log("manusia sebanyak"+" "+manusia)
+    manusia++;
+}
