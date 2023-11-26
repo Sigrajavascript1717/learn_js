ef5a323021b68b50111e1b088f40f411c844c4a9
Sigrajavascript1717 <152014915+Sigrajavascript1717@users.noreply.github.com>
2023-11-26 06:24:19 -0800

    switch

---
+++ soal2.js
@@ -0,0 +1,44 @@
+let tanggal =1
+let bulan = 0
+let tahun = 2000
+switch (bulan) {
+    case 1:
+        console.log(tanggal+" "+"januri"+" "+tahun)
+        break;
+        case 2:
+        console.log(tanggal+" "+"febuari"+" "+tahun)
+        break;
+        case 3:
+        console.log(tanggal+" "+"maret"+" "+tahun)
+        break;
+        case 4:
+        console.log(tanggal+" "+"april"+" "+tahun)
+        break;
+        case 5:
+        console.log(tanggal+" "+"may"+" "+tahun)
+        break;
+        case 6:
+        console.log(tanggal+" "+"juni"+" "+tahun)
+        break;
+        case 7:
+        console.log(tanggal+" "+"juli"+" "+tahun)
+        break;
+        case 8:
+        console.log(tanggal+" "+"agustus"+" "+tahun)
+        break;
+        case 9:
+        console.log(tanggal+" "+"september"+" "+tahun)
+        break;
+        case 10:
+        console.log(tanggal+" "+"oktober"+" "+tahun)
+        break;
+        case 11:
+        console.log(tanggal+" "+"november"+" "+tahun)
+        break;
+        case 12:
+        console.log(tanggal+" "+"desmber"+" "+tahun)
+        break;
+        default:
+        console.log("failed kamu tidak terdaftar jadi bulan wkkkww :)")
+        break;
+}
