53b007e58ee3fa860d0d0684e1083870e57d7efa
Sigrajavascript1717 <152014915+Sigrajavascript1717@users.noreply.github.com>
2023-11-26 23:37:15 +0700

    quiz-soal-part5.5

---
+++ soal_5.5.js
@@ -0,0 +1,90 @@
+console.log("==================================================================================")
+console.log("soal 1")                                                                           
+console.log("==================================================================================")
+console.log("1. Disediakan variabel yg berisi string")
+console.log("2. Harus disatukan menjadi kalimat")
+let word1 = "Javascript"
+let second = "is"
+let third = "awesome"
+let fourth = "and"
+let fifth = "I"
+let sixth = "love"
+let seventh = "it"
+console.log("==================================================================================")
+console.log("maka hasilnya:")
+console.log(word1+" "+second+" "+third+" "+fourth+" "+fifth+" "+sixth+" "+seventh)
+console.log("==================================================================================")
+
+
+
+
+console.log("==================================================================================")
+console.log("soal 2")                                                                           
+console.log("==================================================================================")
+console.log("1. Memecah kalimat dan menampilkan menjadi kata")
+let kata = "hai javascript kamu baik sekali"
+let hai =kata[0]+kata[1]+kata[2]+kata[3]
+let javascript =kata[4]+kata[5]+kata[6]+kata[7]+kata[8]+kata[9]+kata[10]+kata[11]+kata[12]+kata[13]+kata[14]
+let kamu =kata[15]+kata[16]+kata[17]+kata[18]+kata[19]
+let baik= kata[20]+kata[21]+kata[22]+kata[23]+kata[24]
+let sekali=kata[25]+kata[26]+kata[27]+kata[28]+kata[29]+kata[30]
+console.log("==================================================================================")
+console.log("maka hasilnya:")
+console.log(hai)
+console.log(javascript)
+console.log(kamu)
+console.log(baik)
+console.log(sekali)
+console.log("==================================================================================")
+
+
+
+
+console.log("==================================================================================")
+console.log("soal 3")                                                                           
+console.log("==================================================================================")
+console.log("1.Memecah kalimat menggunakan substring")
+let text = "hai javascript kamu baik sekali"
+let halo = text.substring(0,3)
+let kiw =text.substring(4,14)
+let you =text.substring(15,19)
+let me =text.substring(20,24)
+let ouy =text.substring(25,31)
+console.log("==================================================================================")
+console.log("maka hasilnya:")
+console.log(halo)
+console.log(kiw)
+console.log(you)
+console.log(me)
+console.log(ouy)
+console.log("==================================================================================")
+
+
+
+
+
+console.log("==================================================================================")
+console.log("soal 4")                                                                           
+console.log("==================================================================================")
+console.log("1.Memecah kalimat dan menghitung length per kata")
+let isian = "hai javascript kamu baik sekali"
+let isian1 = text.substring(0,3)
+let isian2 =text.substring(4,14)
+let isian3 =text.substring(15,19)
+let isian4 =text.substring(20,24)
+let isian5 =text.substring(25,31)
+let isian_word1 = isian1.length
+let isian_word2 = isian2.length
+let isian_word3 = isian3.length
+let isian_word4 = isian4.length
+let isian_word5 = isian5.length
+console.log("==================================================================================")
+console.log("maka hasilnya:")
+console.log("text:"+" "+isian1+" "+"length nya="+" "+isian_word1)
+console.log("text:"+" "+isian2+" "+"length nya="+" "+isian_word2)
+console.log("text:"+" "+isian3+" "+"length nya="+" "+isian_word3)
+console.log("text:"+" "+isian4+" "+"length nya="+" "+isian_word4)
+console.log("text:"+" "+isian5+" "+"length nya="+" "+isian_word5)
+console.log("==================================================================================")
+
+
