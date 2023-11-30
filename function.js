301073d683c854414077dcf1220051566bb9dce2
Sigrajavascript1717 <152014915+Sigrajavascript1717@users.noreply.github.com>
2023-12-01 01:45:26 +0700

    Add files via upload
    belajar function

---
+++ function.js
@@ -0,0 +1,25 @@
+function shoutOut() {
+    a ="halo Function"
+    return a
+}
+console.log(shoutOut())
+
+function kali_kali(num1,num2) {
+    return num1*num2
+}
+console.log(kali_kali(3,3))
+
+function proses_data() {
+    let nama = "sigra"
+    let umur = 23
+    let alamat ="aceh"
+    let hobi ="belajar ngoding"
+    let hasil ="nama saya"+" "+nama+" "+"umur saya"+" "+umur+" "+"alamat saya"+" "+alamat+" "+"hobi saya"+" "+hobi
+    return hasil
+}
+console.log(proses_data())
+
+
+
+
+
